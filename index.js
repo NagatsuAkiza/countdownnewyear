2405ecf2a2eb9dd60d8f4e054f8b1e085febcc5f
Akiza <74132287+NagatsuAkiza@users.noreply.github.com>
2024-12-31 23:39:33 +0700

    Countdown new years

---
+++ index.js
@@ -0,0 +1,23 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secEl = document.getElementById("sec");
+
+function countdown() {
+  const newYear = new Date("January 1, 2025 00:00:00");
+  const now = new Date();
+  const diff = newYear - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const mins = Math.floor((diff / (1000 * 60)) % 60);
+  const sec = Math.floor((diff / 1000) % 60);
+
+  daysEl.innerText = days;
+  hoursEl.innerText = hours;
+  minsEl.innerText = mins;
+  secEl.innerText = sec;
+}
+
+countdown();
+setInterval(countdown, 1000);
